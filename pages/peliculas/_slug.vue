f3e6e2ec6e124959664f95d2871cd322f7d735a4
TLZ <104867863+1010210@users.noreply.github.com>
2024-10-28 21:16:13 -0600

    Add files via upload

--- pages/peliculas/_slug.vue
+++ pages/peliculas/_slug.vue
@@ -100,7 +100,7 @@ export default {
   mounted() {
     const script = document.createElement("script");
     script.src = "https://utteranc.es/client.js";
-    script.setAttribute("repo", "TefaV/Tarea2");
+    script.setAttribute("repo", "1010210/TAREA2ARQUITECTURA");
     script.setAttribute("issue-term", "pathname");
     script.setAttribute("theme", "github-dark");
     script.setAttribute("crossorigin", "anonymous");
